2bf415538d14a6d78dfe8c0d92a35994047d4908
Zulhardiyanto <dip.achmadz@gmail.com>
2023-10-29 17:29:17 +0700

    StoreStartingApp: load fish target

--- src/store/startingApp.js
+++ src/store/startingApp.js
@@ -1,10 +1,16 @@
 import { defineStore } from "pinia";
 import { useCookies } from "vue3-cookies";
 import { useRouter } from "vue-router";
+import { fishBait, fishEquipment, fishMaster } from "./helpersObject";
+import { getRandomNumber, getRandomPersentage } from "./helpersLogic";
 
 const { cookies } = useCookies();
 export const useStartingAppStore = defineStore({
   id: "StartingApp",
+
+  state: () => ({
+    fishTarget: []
+  }),  
 
   actions: {
     async enterUser(userName) {
@@ -16,9 +22,9 @@ export const useStartingAppStore = defineStore({
       cookies.set("userEnter", JSON.stringify(userSet));
     },
 
-    async getUser () {
-       let userEnter = cookies.get("userEnter")
-       return userEnter 
+    async getUser() {
+      let userEnter = cookies.get("userEnter");
+      return userEnter;
     },
 
     async checkUser() {
@@ -27,6 +33,24 @@ export const useStartingAppStore = defineStore({
       if (cookies.get("userEnter")) {
         router.push({ name: "Dashboard" });
       }
+    },
+
+    async loadFishTarget() {
+      let color = ['red', 'blue', 'green'] 
+      let initPercentage = [10, 20, 30] 
+      let fishingCount = fishEquipment.map((el, i) => {
+        let result = {
+          fishName: el.name,
+          countToday: Math.floor(Math.random() * 10) + 1,
+          fishColor: color[i],
+          percentage: initPercentage[i], //add init percentage
+        };
+        return result;
+      });
+      let getPercentageFish = getRandomPersentage(fishingCount)
+      this.fishTarget = getPercentageFish;
+      
+      return getPercentageFish;
     },
   },
 });
