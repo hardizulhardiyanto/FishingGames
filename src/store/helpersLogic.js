2811a3d810b15f1c765a32aafc041151f77ee9f9
Zulhardiyanto <dip.achmadz@gmail.com>
2023-10-29 17:28:48 +0700

    Store: Add Helpers

---
+++ src/store/helpersLogic.js
@@ -0,0 +1,30 @@
+// getRandomPersentage From Object
+export const getRandomPersentage = (objects) => {
+  // Calculate the total percentage of all objects
+  let totalPercentage = objects.reduce(function (total, obj) {
+    return total + obj.percentage;
+  }, 0);
+
+  // Randomly distribute the remaining percentage to objects
+  while (totalPercentage < 100) {
+    const availableObjects = objects.filter(function (obj) {
+      return obj.percentage < 100;
+    });
+    const randomIndex = Math.floor(Math.random() * availableObjects.length);
+    const increment = Math.min(
+      100 - totalPercentage,
+      100 - availableObjects[randomIndex].percentage
+    );
+    availableObjects[randomIndex].percentage += increment;
+    totalPercentage += increment;
+  }
+
+  return objects;
+};
+
+// Generate a random number among 10, 20, or 30
+export const getRandomNumber = ()  =>{
+    const options = [10, 20, 30];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
---
+++ src/store/helpersObject.js
@@ -0,0 +1,83 @@
+export const fishEquipment = [
+    {
+        name: "small",
+        price: 5,
+        fishCatch: "small"
+    },
+    {
+        name: "medium",
+        price: 10,
+        fishCatch: "medium"
+    },
+    {
+        name: "large",
+        price: 15,
+        fishCatch: "large"
+    }
+]
+
+export const fishBait = [
+    {
+        name: "red",
+        price: 1,
+        fishCatch: 1
+    },
+    {
+        name: "blue",
+        price: 2,
+        fishCatch: 1
+    },
+    {
+        name: "green",
+        price: 3,
+        fishCatch: 1
+    },
+]
+
+export const fishMaster = [
+    {
+        name: "small",
+        sellPrice: "1-5",
+        color: "red"
+    },
+    {
+        name: "medium",
+        sellPrice: "5-10",
+        color: "red"
+    },
+    {
+        name: "large",
+        sellPrice: "10-15",
+        color: "red"
+    },
+    {
+        name: "small",
+        sellPrice: "3-5",
+        color: "blue"
+    },
+    {
+        name: "medium",
+        sellPrice: "8-10",
+        color: "blue"
+    },
+    {
+        name: "large",
+        sellPrice: "13-15",
+        color: "blue"
+    },
+    {
+        name: "small",
+        sellPrice: "5",
+        color: "green"
+    },
+    {
+        name: "medium",
+        sellPrice: "10",
+        color: "green"
+    },
+    {
+        name: "large",
+        sellPrice: "15",
+        color: "green"
+    }
+]
