5a299b1828c20e40d5e3a2e4d0c3489bca068e04
Zulhardiyanto <dip.achmadz@gmail.com>
2023-10-29 17:56:56 +0700

    UserInfo: added

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container class="bg-white userinfo__container">
+        <p class="text-subtitle-1">{{ props.username }}</p>
+        <p class="text-subtitle-2">{{ 'your gold: '+ props.gold }}</p>
+    </v-container>
+</template>
+
+<script setup>
+const props = defineProps({
+    username: {
+        type: String,
+        default: () => "Insert Username Props"
+    },
+    gold: {
+        type: Number,
+        default: () => 0
+    }
+})
+
+</script>
+
+<style lang="scss" scoped>
+.userinfo__container {
+    border: 3px;
+    border-radius: 3px;
+}
+</style>
