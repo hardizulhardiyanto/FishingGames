e2dc0cf0c2f80acd3c141f777f67de0995a4145a
Zulhardiyanto <dip.achmadz@gmail.com>
2023-10-31 10:08:11 +0700

    StartingApp: fixed

--- src/components/StartingApp.vue
+++ src/components/StartingApp.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="d-flex justify-center" fluid style="height: 100%">
+  <v-container class="d-flex justify-center align-center" fluid style="height: 100%">
     <v-card elevation="4" class="startingapp__player">
       <v-card-title class="bg-white"> Welcome Fishing Games </v-card-title>
       <v-card-item>
